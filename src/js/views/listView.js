62f6b5e0eeb737f5721dcf4d3fe4d2321b30d4ff
Kelechukwu Tasie <ktasie@gmail.com>
2020-03-27 12:54:39 +0100

    shopping list view

--- src/js/views/listView.js
+++ src/js/views/listView.js
@@ -15,9 +15,10 @@ export const renderItem = item => {
             </button>
         </li>
     `;
-    elements.shopping.insertAdjacentHTML('beforeend', markup); 
+    elements.shopping.insertAdjacentHTML('beforeend', markup);  
 }
 
 export const deleteItem = id => {
-
+    const item = document.querySelector(`[data-itemid="${id}"]`);
+    item.parentElement.removeChild(item);
 }
